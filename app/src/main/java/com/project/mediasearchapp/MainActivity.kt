9005d2966aa8327c477c86f5bc50d79c427b0b78
이승준 <6951004@gmail.com>
2022-03-19 17:24:35 +0900

    [feat] 기본 틀 푸쉬

---
+++ app/src/main/java/com/project/mediasearchapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.project.mediasearchapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.mediasearchapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
